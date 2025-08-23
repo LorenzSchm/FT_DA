e3c604f088d9f9cc885054c5e4c932f5b0f4d26a
Lorenz191 <144610453+Lorenz191@users.noreply.github.com>
2025-08-23 10:53:19 +0200

    feat(website): started w website

--- webapp/src/app/components/navigation/NavBar.jsx
+++ webapp/src/app/components/navigation/NavBar.jsx
@@ -1,21 +1,15 @@
 export default function NavBar() {
-    return (
-        <div className="w-full bg-gradient-to-b from-[#B7B7B7] to-transparent flex flex-row justify-between items-center">
-            <div className={"p-4 flex flex-row items-center  gap-2"}>
-                <img src="/icon.svg" alt="logo" width={25}/>
-                <h1 className={"text-white font-bold text-3xl"}>Finance Tracker</h1>
-            </div>
-            <div className="flex flex-row items-center h-fit pl-3 pr-3 gap-6 m-4 text-white font-bold bg-white/5 backdrop-blur-md border border-white/30 rounded-3xl">
-                <div>
-                    About
-                </div>
-                <div>
-                    Plans
-                </div>
-                <div>
-                    The Team
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full bg-gradient-to-b from-[#B7B7B7] to-transparent flex flex-row justify-between items-center">
+      <div className={"p-4 flex flex-row items-center  gap-2"}>
+        <img src="/icon.svg" alt="logo" width={25} />
+        <h1 className={"text-white font-bold text-3xl"}>Finance Tracker</h1>
+      </div>
+      <div className="flex flex-row items-center h-fit pl-3 pr-3 gap-6 m-4 text-white font-bold bg-white/5 backdrop-blur-md border border-white/30 rounded-3xl">
+        <div>About</div>
+        <div>Plans</div>
+        <div>The Team</div>
+      </div>
+    </div>
+  );
 }
--- webapp/src/app/page.js
+++ webapp/src/app/page.js
@@ -2,10 +2,13 @@ import NavBar from "@/app/components/navigation/NavBar";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-cover bg-no-repeat overflow-hidden" style={{backgroundImage: "url('/Updated_ny.jpg')"}}>
-        <div>
-            <NavBar />
-        </div>
+    <div
+      className="w-full h-screen bg-cover bg-no-repeat overflow-hidden"
+      style={{ backgroundImage: "url('/Updated_ny.jpg')" }}
+    >
+      <div>
+        <NavBar />
+      </div>
     </div>
   );
 }
