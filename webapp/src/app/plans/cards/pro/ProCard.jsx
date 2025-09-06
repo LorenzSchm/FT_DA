901175acfa628c505bd1daab8ae88b493f2ce3cd
pseytter <1053@htl.rennweg.at>
2025-09-06 10:10:14 +0200

    feat(all): finished PlansView(please add dots)

--- webapp/src/app/plans/cards/pro/ProCard.jsx
+++ webapp/src/app/plans/cards/pro/ProCard.jsx
@@ -4,14 +4,14 @@ import { CheckCircle } from "react-feather";
 export default function ProCard() {
   const data = {
     title: "Pro",
-      subtitle:"T.B.A."
+    subtitle: "T.B.A.",
   };
 
   return (
     <div className="w-[350px] h-[530px] border-2 border-gray-200 rounded-[50px] px-6 pt-3 pb-8 flex flex-col justify-between">
       <div className="flex flex-col mt-6 gap-5">
         <div className="font-bold text-3xl">{data.title}</div>
-          <div className="font-bold text-3xl">{data.subtitle}</div>
+        <div className="font-bold text-3xl">{data.subtitle}</div>
       </div>
     </div>
   );
