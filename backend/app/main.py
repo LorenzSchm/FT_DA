c4d9b72c4763ec0ee599271cfa145a94064ac422
Lorenz191 <144610453+Lorenz191@users.noreply.github.com>
2025-10-07 21:35:56 +0200

    feat(backend): added all api routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from .routers.account.account import router as account_router
+from .routers.auth.auth import router as auth_router
+from .routers.savings.savings import router as savings_router
+from .routers.bank_connection.bank_connection import router as bank_connection_router
+from .routers.bot.bot import router as bot_router
+from .routers.invetsments.investments import router as investments_router
+from .routers.transactions.transactions import router as transactions_router
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app = FastAPI()
+app.include_router(account_router)
+app.include_router(auth_router)
+app.include_router(savings_router)
+app.include_router(bank_connection_router)
+app.include_router(bot_router)
+app.include_router(investments_router)
+app.include_router(transactions_router)
