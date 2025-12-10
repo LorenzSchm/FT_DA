b01990e946c66eb81a5f9d36563b249bd0557693
pseytter <1053@htl.rennweg.at>
2025-12-10 21:56:52 +0100

    feat(frontend/backend): pulled from main

--- mobile/utils/db/connect_accounts/connectAccounts.ts
+++ mobile/utils/db/connect_accounts/connectAccounts.ts
@@ -2,28 +2,37 @@ import axios from "axios";
 
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:8000";
 
-export const generateToken = async (accessToken:string , refreshToken: string, code: string, name: string) => {
-    if (!accessToken || !refreshToken || !code) {
-        throw new Error("Missing access token, refresh token or code");
-    }
-    const resp = await axios.post(`${BASE_URL}/bank/generate-token`, {code, name}, {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "x-refresh-token": refreshToken,
-        }
-    });
-    return resp.data;
-}
+export const generateToken = async (
+  accessToken: string,
+  refreshToken: string,
+  code: string,
+  name: string,
+) => {
+  if (!accessToken || !refreshToken || !code) {
+    throw new Error("Missing access token, refresh token or code");
+  }
+  const resp = await axios.post(
+    `${BASE_URL}/bank/generate-token`,
+    { code, name },
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "x-refresh-token": refreshToken,
+      },
+    },
+  );
+  return resp.data;
+};
 
 export const getData = async (accessToken: string, refreshToken: string) => {
-    if (!accessToken) {
-        throw new Error("Missing access token");
-    }
-    const resp = await axios.get(`${BASE_URL}/bank/data`, {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "x-refresh-token": refreshToken,
-        }
-    });
-    return resp.data;
-}
+  if (!accessToken) {
+    throw new Error("Missing access token");
+  }
+  const resp = await axios.get(`${BASE_URL}/bank/data`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "x-refresh-token": refreshToken,
+    },
+  });
+  return resp.data;
+};
